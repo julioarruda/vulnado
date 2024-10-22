b58dce028e7db1595340fd1dd39acd738ba68ade
Julio Arruda <julio.arruda@outlook.com>
2024-10-22 20:18:02 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,18 +1,24 @@
 package com.scalesec.vulnado;
+import java.util.logging.Logger;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Cowsay {
+  private static final Logger logger = Logger.getLogger(Cowsay.class.getName());
+  public static String run(String input) {
+  private Cowsay() {
+    ProcessBuilder processBuilder = new ProcessBuilder();
+    // Hide the implicit public constructor
+    String cmd = "/usr/games/cowsay '" + input + "'";
+  }
+    System.out.println(cmd);
   public static String run(String input) {
     ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
-
+    String sanitizedInput = input.replaceAll("[^a-zA-Z0-9]", ""); // Simple sanitization
     StringBuilder output = new StringBuilder();
-
-    try {
+    String cmd = "/usr/games/cowsay '" + sanitizedInput + "'";
+    logger.info(cmd); // Use logger instead of System.out.println
       Process process = processBuilder.start();
       BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
@@ -21,7 +27,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      logger.severe(e.getMessage()); // Use logger instead of e.printStackTrace()
     }
     return output.toString();
   }
