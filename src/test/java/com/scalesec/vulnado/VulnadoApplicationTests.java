1cb0dc3a1233eeb057bc5dbd1551800877496e21
Julio Arruda <julio.arruda@outlook.com>
2024-10-22 20:07:10 -0300

    [GFTCodeFixer]: Update on src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -1,17 +1,22 @@
 package com.scalesec.vulnado;
 
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VulnadoApplicationTests {
+    @Autowired
 
+    private ApplicationContext applicationContext;
 	@Test
 	public void contextLoads() {
-	}
+        assertNotNull(applicationContext);
 
 }
 
