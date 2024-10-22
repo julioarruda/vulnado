c25a97496a20c3f9d843b59b3048f447f1b7db1c
Julio Arruda <julio.arruda@outlook.com>
2024-10-22 19:53:33 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,13 +2,21 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
-public class Cowsay {
+import java.util.logging.Level;
+public final class Cowsay {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
   public static String run(String input) {
+
     ProcessBuilder processBuilder = new ProcessBuilder();
+  private Cowsay() {
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Private constructor to hide the implicit public one
+    LOGGER.info(cmd);
+  }
+    processBuilder.command("bash", "-c", "/usr/games/cowsay", input);
+
 
     StringBuilder output = new StringBuilder();
 
@@ -21,7 +29,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      LOGGER.log(Level.SEVERE, "An error occurred", e);
     }
     return output.toString();
   }
