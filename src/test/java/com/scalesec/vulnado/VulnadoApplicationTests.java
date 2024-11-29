a1486b90574f588a0590c13cb18c334984dc3550
Julio Arruda <julio.arruda@outlook.com>
2024-11-29 15:32:57 -0300

    [GFTCodeFixer]: Update on src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java

--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -1,6 +1,8 @@
 package com.scalesec.vulnado;
 
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
+import org.springframework.boot.SpringApplication;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,6 +13,7 @@ public class VulnadoApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		assertNotNull("Application context should load", SpringApplication.run(VulnadoApplication.class));
 	}
 
 }
