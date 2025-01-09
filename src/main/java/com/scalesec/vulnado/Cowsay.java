f72dc8441c63ac4cfca3766671a41424d84f6153
Julio Arruda <julio.arruda@outlook.com>
2025-01-09 13:21:19 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -1,17 +1,31 @@
 package com.scalesec.vulnado;
 
+import java.util.ArrayList;
 import java.io.BufferedReader;
+import java.util.List;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
-public class Cowsay {
+import java.util.logging.Level;
+public final class Cowsay {
+  private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
   public static String run(String input) {
+
     ProcessBuilder processBuilder = new ProcessBuilder();
+  private Cowsay() {
     String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+    // Private constructor to hide the implicit public one
+    LOGGER.info(cmd);
+  }
+    List<String> command = new ArrayList<>();
+    command.add("bash");
 
+
+    command.add("-c");
     StringBuilder output = new StringBuilder();
+    command.add(cmd);
 
+    processBuilder.command(command);
     try {
       Process process = processBuilder.start();
       BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -21,7 +35,7 @@ public class Cowsay {
         output.append(line + "\n");
       }
     } catch (Exception e) {
-      e.printStackTrace();
+      LOGGER.log(Level.SEVERE, "Error executing cowsay command", e);
     }
     return output.toString();
   }
