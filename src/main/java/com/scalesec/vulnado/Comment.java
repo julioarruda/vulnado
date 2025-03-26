1a084716e2c43e650d39dd8a89b4c12f643ce236
Julio Arruda <julio.arruda@gft.com>
2025-03-25 21:34:43 -0300

    fix: corrige 43 issues do SonarQube

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -1,86 +1,91 @@
 package com.scalesec.vulnado;
 
-import org.apache.catalina.Server;
 import java.sql.*;
 import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Comment {
-  public String id, username, body;
-  public Timestamp created_on;
+  private static final Logger logger = Logger.getLogger(Comment.class.getName());
 
-  public Comment(String id, String username, String body, Timestamp created_on) {
+  public String id;
+  public String username;
+  public String body;
+  private final Timestamp createdOn;
+
+  public Comment(String id, String username, String body, Timestamp createdOn) {
     this.id = id;
     this.username = username;
     this.body = body;
-    this.created_on = created_on;
+    this.createdOn = createdOn;
   }
 
-  public static Comment create(String username, String body){
-    long time = new Date().getTime();
-    Timestamp timestamp = new Timestamp(time);
-    Comment comment = new Comment(UUID.randomUUID().toString(), username, body, timestamp);
+  public Timestamp getCreatedOn() {
+    return createdOn;
+  }
+
+  public static Comment createComment(String username, String body){
+    long currentTime = new Date().getTime();
+    Timestamp currentTimestamp = new Timestamp(currentTime);
+    Comment newComment = new Comment(UUID.randomUUID().toString(), username, body, currentTimestamp);
     try {
-      if (comment.commit()) {
-        return comment;
+      if (newComment.commitComment()) {
+        return newComment;
       } else {
         throw new BadRequest("Unable to save comment");
       }
     } catch (Exception e) {
+      logger.log(Level.SEVERE, "Error creating comment", e);
       throw new ServerError(e.getMessage());
     }
   }
 
-  public static List<Comment> fetch_all() {
-    Statement stmt = null;
-    List<Comment> comments = new ArrayList();
-    try {
-      Connection cxn = Postgres.connection();
-      stmt = cxn.createStatement();
+  public static List<Comment> fetchAllComments() {
+    Statement statement = null;
+    List<Comment> comments = new ArrayList<>();
+    try (Connection connection = Postgres.connection()) {
+      statement = connection.createStatement();
 
-      String query = "select * from comments;";
-      ResultSet rs = stmt.executeQuery(query);
-      while (rs.next()) {
-        String id = rs.getString("id");
-        String username = rs.getString("username");
-        String body = rs.getString("body");
-        Timestamp created_on = rs.getTimestamp("created_on");
-        Comment c = new Comment(id, username, body, created_on);
-        comments.add(c);
+      String query = "SELECT * FROM comments;";
+      ResultSet resultSet = statement.executeQuery(query);
+      while (resultSet.next()) {
+        String commentId = resultSet.getString("id");
+        String commentUsername = resultSet.getString("username");
+        String commentBody = resultSet.getString("body");
+        Timestamp commentCreatedOn = resultSet.getTimestamp("created_on");
+        Comment comment = new Comment(commentId, commentUsername, commentBody, commentCreatedOn);
+        comments.add(comment);
       }
-      cxn.close();
     } catch (Exception e) {
-      e.printStackTrace();
-      System.err.println(e.getClass().getName()+": "+e.getMessage());
-    } finally {
-      return comments;
+      logger.log(Level.SEVERE, "Error fetching comments", e);
     }
+    return comments;
   }
 
-  public static Boolean delete(String id) {
-    try {
-      String sql = "DELETE FROM comments where id = ?";
-      Connection con = Postgres.connection();
-      PreparedStatement pStatement = con.prepareStatement(sql);
-      pStatement.setString(1, id);
-      return 1 == pStatement.executeUpdate();
+  public static boolean deleteComment(String id) {
+    String sql = "DELETE FROM comments WHERE id = ?";
+    try (Connection connection = Postgres.connection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, id);
+      return 1 == preparedStatement.executeUpdate();
     } catch(Exception e) {
-      e.printStackTrace();
-    } finally {
-      return false;
+      logger.log(Level.SEVERE, "Error deleting comment", e);
     }
+    return false;
   }
 
-  private Boolean commit() throws SQLException {
+  private boolean commitComment() throws SQLException {
     String sql = "INSERT INTO comments (id, username, body, created_on) VALUES (?,?,?,?)";
-    Connection con = Postgres.connection();
-    PreparedStatement pStatement = con.prepareStatement(sql);
-    pStatement.setString(1, this.id);
-    pStatement.setString(2, this.username);
-    pStatement.setString(3, this.body);
-    pStatement.setTimestamp(4, this.created_on);
-    return 1 == pStatement.executeUpdate();
+    try (Connection connection = Postgres.connection();
+         PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+      preparedStatement.setString(1, this.id);
+      preparedStatement.setString(2, this.username);
+      preparedStatement.setString(3, this.body);
+      preparedStatement.setTimestamp(4, this.createdOn);
+      return 1 == preparedStatement.executeUpdate();
+    }
   }
 }
--- src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
+++ src/test/java/com/scalesec/vulnado/VulnadoApplicationTests.java
@@ -2,16 +2,22 @@ package com.scalesec.vulnado;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class VulnadoApplicationTests {
 
+	@Autowired
+	private ApplicationContext applicationContext;
+
 	@Test
 	public void contextLoads() {
+		assertNotNull("The application context should have loaded.", applicationContext);
 	}
-
 }
-
