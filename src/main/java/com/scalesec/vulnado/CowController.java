4db653643a25c5945628d1c6a07424e632a28dcd
Julio Arruda <julio.arruda@outlook.com>
2025-04-23 16:18:38 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/CowController.java

--- src/main/java/com/scalesec/vulnado/CowController.java
+++ src/main/java/com/scalesec/vulnado/CowController.java
@@ -3,12 +3,12 @@ package com.scalesec.vulnado;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
 
-import java.io.Serializable;
 
 @RestController
 @EnableAutoConfiguration
 public class CowController {
-    @RequestMapping(value = "/cowsay")
+    @RequestMapping(value = "/cowsay", method = RequestMethod.GET)
+    // TODO: Review allowed HTTP methods for security. Currently allows all methods (GET, POST, etc.).
     String cowsay(@RequestParam(defaultValue = "I love Linux!") String input) {
         return Cowsay.run(input);
     }
