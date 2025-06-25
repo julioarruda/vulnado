be1d4613b4660c42d8551ad925844ad2e11565af
Julio Arruda <julio.arruda@outlook.com>
2025-06-25 17:29:03 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,10 +1,10 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
+import org.springframework.beans.factory.annotation.*;
+import java.io.Serializable;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -14,11 +14,11 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = "*") // NOSONAR - Make sure this CORS configuration is safe for production
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    User user = User.fetch(input.getUsername());
+    if (Postgres.md5(input.getPassword()).equals(user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");
@@ -27,14 +27,20 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+  private String username;
+  private String password;
+  public String getUsername() { return username; }
 }
+  public void setUsername(String username) { this.username = username; }
 
+  public String getPassword() { return password; }
 class LoginResponse implements Serializable {
-  public String token;
+  public void setPassword(String password) { this.password = password; }
+  private String token;
   public LoginResponse(String msg) { this.token = msg; }
+  public String getToken() { return token; }
 }
+  public void setToken(String token) { this.token = token; }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class Unauthorized extends RuntimeException {
