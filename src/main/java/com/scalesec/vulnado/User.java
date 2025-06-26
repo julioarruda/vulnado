ce15d2fce425ee80325e10349b0b8c7aca8650ef
Julio Arruda <julio.arruda@outlook.com>
2025-06-26 09:26:32 -0300

    Update User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -20,9 +20,7 @@ public class User {
 
   public String token(String secret) {
     SecretKey key = Keys.hmacShaKeyFor(secret.getBytes());
-    String jws = Jwts.builder().setSubject(this.username).signWith(key).compact();
-    return jws;
-  }
+    String jws = Jwts.builder().setSubject(this.username).signWit
 
   public static void assertAuth(String secret, String token) {
     try {
