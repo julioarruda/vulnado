42477ed448c271c3ebe79f1b4d298650cc0f7188
Julio Arruda <julio.arruda@outlook.com>
2025-08-05 16:35:37 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -11,34 +11,34 @@ import java.io.Serializable;
 @RestController
 @EnableAutoConfiguration
 public class LoginController {
-  @Value("${app.secret}")
-  private String secret;
+    @Value("${app.secret}")
+    private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
-  LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
-      return new LoginResponse(user.token(secret));
-    } else {
-      throw new Unauthorized("Access Denied");
+    @CrossOrigin(origins = "*")
+    @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+    public LoginResponse login(@RequestBody LoginRequest input) {
+        User user = User.fetch(input.username);
+        if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+            return new LoginResponse(user.token(secret));
+        } else {
+            throw new Unauthorized("Access Denied");
+        }
     }
-  }
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+    public String username;
+    public String password;
 }
 
 class LoginResponse implements Serializable {
-  public String token;
-  public LoginResponse(String msg) { this.token = msg; }
+    public String token;
+    public LoginResponse(String msg) { this.token = msg; }
 }
 
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
 class Unauthorized extends RuntimeException {
-  public Unauthorized(String exception) {
-    super(exception);
-  }
+    public Unauthorized(String exception) {
+        super(exception);
+    }
 }
