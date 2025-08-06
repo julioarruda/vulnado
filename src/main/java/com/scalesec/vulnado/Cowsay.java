9e0429f994e7b22318e169f3aae4c537d6c53338
Julio Arruda <julio.arruda@outlook.com>
2025-08-06 10:37:03 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/Cowsay.java

--- src/main/java/com/scalesec/vulnado/Cowsay.java
+++ src/main/java/com/scalesec/vulnado/Cowsay.java
@@ -2,27 +2,28 @@ package com.scalesec.vulnado;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.logging.Logger;
 
-public class Cowsay {
-  public static String run(String input) {
-    ProcessBuilder processBuilder = new ProcessBuilder();
-    String cmd = "/usr/games/cowsay '" + input + "'";
-    System.out.println(cmd);
-    processBuilder.command("bash", "-c", cmd);
+  public class Cowsay {
+    private static final Logger LOGGER = Logger.getLogger(Cowsay.class.getName());
 
-    StringBuilder output = new StringBuilder();
-
-    try {
-      Process process = processBuilder.start();
-      BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
-
-      String line;
-      while ((line = reader.readLine()) != null) {
-        output.append(line + "\n");
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
+    private Cowsay() {
+        // Private constructor to hide implicit public one
     }
-    return output.toString();
-  }
-}
+
+    public static String run(String input) {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        String cmd = "/usr/games/cowsay " + input;
+        LOGGER.info("Executing command: " + cmd);
+        processBuilder.command("/usr/games/cowsay", input);
+
+        StringBuilder output = new StringBuilder();
+
+        try {
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
