063ecc7f60ee1f36c97da0ebdedd3d3051c2ce1e
Julio Arruda <julio.arruda@outlook.com>
2025-10-03 16:36:27 -0300

    [GFTCodeFixer]: Update on src/main/java/com/scalesec/vulnado/LoginController.java

--- src/main/java/com/scalesec/vulnado/LoginController.java
+++ src/main/java/com/scalesec/vulnado/LoginController.java
@@ -1,10 +1,8 @@
 package com.scalesec.vulnado;
 
-import org.springframework.boot.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.beans.factory.annotation.*;
 import java.io.Serializable;
 
@@ -14,11 +12,11 @@ public class LoginController {
   @Value("${app.secret}")
   private String secret;
 
-  @CrossOrigin(origins = "*")
-  @RequestMapping(value = "/login", method = RequestMethod.POST, produces = "application/json", consumes = "application/json")
+  @CrossOrigin(origins = "http://localhost:8080") // Restrict CORS to specific origin
+  @PostMapping(value = "/login", produces = "application/json", consumes = "application/json")
   LoginResponse login(@RequestBody LoginRequest input) {
-    User user = User.fetch(input.username);
-    if (Postgres.md5(input.password).equals(user.hashedPassword)) {
+    User user = User.fetch(input.getUsername());
+    if (Postgres.md5(input.getPassword()).equals(user.hashedPassword)) {
       return new LoginResponse(user.token(secret));
     } else {
       throw new Unauthorized("Access Denied");
@@ -27,18 +25,36 @@ public class LoginController {
 }
 
 class LoginRequest implements Serializable {
-  public String username;
-  public String password;
+  private String username;
+  private String password;
+  
 }
+  public String getUsername() {
 
+    return username;
 class LoginResponse implements Serializable {
-  public String token;
-  public LoginResponse(String msg) { this.token = msg; }
-}
+  }
+  private String token;
 
+  public LoginResponse(String msg) { this.token = msg; }
+  public String getToken() {
+  public void setUsername(String username) {
+}
+    return token;
+    this.username = username;
+
+  }
 @ResponseStatus(HttpStatus.UNAUTHORIZED)
+
 class Unauthorized extends RuntimeException {
+  public void setToken(String token) {
+  public String getPassword() {
   public Unauthorized(String exception) {
+    this.token = token;
+    return password;
     super(exception);
   }
+  }
+
 }
+  public void setPassword(String password) {
